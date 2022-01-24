64eb4d21a16130863e73aada69bb65587c33eca4
sou <sasaki.sou@samurai-adways.net>
2022-01-24 21:23:07 +0900

    add: stories for room

--- src/components/RoomGameStartButton.tsx
+++ src/components/RoomGameStartButton.tsx
@@ -1,31 +1,39 @@
 import BaseButton from "./BaseButton";
-import { useState } from "react";
+import React, { useState } from "react";
+import { Member } from "../models/member";
 
-export default function RoomGameStartButton(props: any) {
-	const members = props.members;
-	const startGame = props.startGame;
+type Props = {
+  members: Member[];
+  startGame: (flag: boolean) => {};
+};
 
-	const handleClick = () => {
-		setIsModalOpen(true);
-		// startGame(true);
-	}
-	// モーダル表示のみなので、できればCSSで行いたいが
-	const [isModalOpen, setIsModalOpen] = useState(false);
-	const handleStart = (e: any) => {
-		e.preventDefault();
-		startGame(true);
-	}
-	const handleCancel = (e: any) => {
-		e.preventDefault();
-		setIsModalOpen(false);
-	}
-	if (members.length >= 4) {
-		return (
-			<div>
-				<BaseButton text="Start!" onClick={handleClick} />
+export const RoomGameStartButton: React.VFC<Props> = (props: any) => {
+  const members = props.members;
+  const startGame = props.startGame;
 
-				<div id="start-modal" className={`
-					${isModalOpen ? 'block' : 'hidden'}
+  const handleClick = () => {
+    setIsModalOpen(true);
+    // startGame(true);
+  };
+  // モーダル表示のみなので、できればCSSで行いたいが
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleStart = (e: any) => {
+    e.preventDefault();
+    startGame(true);
+  };
+  const handleCancel = (e: any) => {
+    e.preventDefault();
+    setIsModalOpen(false);
+  };
+  if (members.length >= 4) {
+    return (
+      <div>
+        <BaseButton text="Start!" onClick={handleClick} />
+
+        <div
+          id="start-modal"
+          className={`
+					${isModalOpen ? "block" : "hidden"}
 					animate-fade-in
 					transition-opacity
 					duration-500
@@ -34,21 +42,22 @@ export default function RoomGameStartButton(props: any) {
 					left-0
 					bg-gray-200
 					bg-opacity-60
-				`}>
-					<div className="w-screen h-screen flex flex-col justify-center">
-						<div className="grid gap-4">
-							<h4 className="text-center">Start Game?</h4>
-							<div className="grid grid-cols-2 gap-4">
-								<BaseButton text="Yes" onClick={handleStart} />
-								<BaseButton text="No" onClick={handleCancel}/>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		)
-	} else {
-		// TODO: what should I return?
-		return <div></div>;
-	}
-}
+				`}
+        >
+          <div className="w-screen h-screen flex flex-col justify-center">
+            <div className="grid gap-4">
+              <h4 className="text-center">Start Game?</h4>
+              <div className="grid grid-cols-2 gap-4">
+                <BaseButton text="Yes" onClick={handleStart} />
+                <BaseButton text="No" onClick={handleCancel} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    // TODO: what should I return?
+    return <div></div>;
+  }
+};
--- src/components/RoomUserInfo.tsx
+++ src/components/RoomUserInfo.tsx
@@ -1,4 +1,11 @@
-export default function RoomUserInfo(props: any) {
+import React from "react";
+import { UserInfo } from "../models/UserInfo";
+
+type Props = {
+    userInfo: UserInfo
+}
+
+export const RoomUserInfo: React.VFC<Props> = (props) => {
     const userInfo = props.userInfo;
     return (
         <div className="container mx-auto flex-col justify-center items-center">
